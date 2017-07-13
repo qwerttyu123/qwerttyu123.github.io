eda1985b5d3fd38079e9ca91f772e501b8650631
mirkir <mole@p33.org>
2017-07-13 21:57:00 +0700

    ds
    f

---
+++ js/script.js
@@ -0,0 +1,14 @@
+	$(document).ready(function() {  			
+    	$(".main").onepage_scroll({
+     		sectionContainer: "section"
+    	});
+ 			
+		$('.info').particleground({
+				dotColor: '#E1D0D0',
+				lineColor: '#B0AFAF',
+				parallax: false,
+				lineWidth: 0.5,
+				particleRadius:5,
+				density: 7000
+		});
+	});
